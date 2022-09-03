ca10c5f3883d13a8435e559c87f0bb0a9896e9d3
PedroLeGarcia <91338154+PedroLeGarcia@users.noreply.github.com>
2022-09-03 05:10:46 -0300

    Update home.php

--- home.php
+++ home.php
@@ -10,7 +10,8 @@ include('biblioteca.php');
 	color: #fff;
 }
 body{
-    min-height: 100vh;
+	width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -18,20 +19,83 @@ body{
     background: -webkit-linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9);
     background: linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9); 
 }
-.tittle{
-	
+.container{
+	display: flex;
+}
+.form{
+	width: 100%;
+	display: flex;
+	justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 3rem;
+}
+.title{
+	display: flex;
+	justify-content: center;
+}
+.title h1{
+	margin-bottom: 0.5rem;
+}
+.title h2{
+
+}
+.input-group{
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+}
+.input-box{
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 0.5rem
+}
+.input-box input{
+	margin: 0.1rem 0;
+	padding: 10px 60px;
+	border: 1px solid #fff;
+	border-radius: 5px;
+	background-color: #fff;
+	box-shadow: 1px 1px #0000001c;
+	transition: 0.5s;
+	color: #1E90FF;
+	text-align: center;
+}
+.input-box input:hover{
+	background-color: #eeeeee75;
+}
+.bnt button{
+	padding: 10px 100px;
+	font-size: 20px;
+	background-color: #fff;
+	border: 1px solid #fff;
+	border-radius: 5px;
+	cursor: pointer;
+	color: #1E90FF;
+	transition: 0.5s;
+}
+.bnt button:hover{
+	background-color: #1E90FF;
+	color: #fff;
+}
+table tr td{
+	color: #1E90FF;
 }
 </style>
 <div class="container">
-	<div class="tittle">
-		<h1>Olá <?php echo $_SESSION['nome']; ?>! </h1>
-	</div>
-
-	<div class="form-novoGenero">
+	<div class="form"><br>
+		<div class="form-header">
+			<div class="title"> <h1>Olá! </h1> </div>
+		</div>
+	<div class="input-group">
 		<form id="novoGenero" method="post">
-				<h2>Novo Genêro</h2><br>
-					<input type="text" name="genero" placeholder="Nome do Genêro..."><br><br>
-				<button>Cadatrar</button>
+			<div class="input-box">
+				<div class="title"><h2>Novo Genêro</h2></div>
+				<input type="text" name="genero" placeholder="Nome do Genêro...">
+			</div>
+			<div class="bnt">
+				<button>Cadastrar</button>
+			</div>
 		</form>
 <?php
 	if(isset($_POST['genero'])){
@@ -58,15 +122,17 @@ body{
 				</tr>';		}
 	?>
 	</table>
-	</div> <!--fim do form-novoGenero-->
+	</div> <!--fim do input-group-->
 
-	<br><hr><br>
-
-	<div class="form-autor">
+	<div class="input-group">
 		<form id="novoAutor" method="post">
-			<h2>Novo autor</h2><br>
-			<input type="text" name="autor" placeholder="Nome do Autor..."><br><br>
-			<button>Cadatrar</button>
+			<div class="input-box">
+				<div class="title"><h2>Novo Autor</h2></div>
+				<input type="text" name="autor" placeholder="Nome do Autor...">
+			</div>
+			<div class="bnt">
+				<button>Cadastrar</button>
+			</div>
 		</form>
 <?php
 	if(isset($_POST['autor'])){
@@ -94,16 +160,19 @@ body{
 				</tr>';		}
 	?>
 	</table>
-	</div><!--fim do form-autor-->
+	</div>
 	
-	<br><hr><br>
 
-	<div class="form-editora">
-	<form id="novaEditora" method="post"><br>
-		<h2>Nova Editora</h2><br>
-		<input type="text" name="editora" placeholder="Nome da Editora..."><br><br>
-		<button>Cadatrar</button>
-	</form>
+	<div class="input-group">
+		<form id="novaEditora" method="post">
+			<div class="input-box">
+				<div class="title"><h2>Nova Editora</h2></div>
+				<input type="text" name="editora" placeholder="Nome da Editora...">
+			</div>
+			<div class="bnt">
+				<button>Cadastrar</button>
+			</div>
+		</form>
 <?php
 	if(isset($_POST['editora'])){
 		CadastrarEditora($_POST['editora']);
@@ -131,5 +200,6 @@ body{
 	?>
 	</table>
 	<br><hr><br>
+		</div><!--fim do input-group-->
 	</div>
 </div>
